ed34038861bf568ff056f1a8008ff9cddf38c2c3
Mar Ortiz Velasquez <mariaaortizv@gmail.com>
2022-10-22 11:33:43 -0500

    estoy triunfando

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -5,7 +5,7 @@ public class Calculadora {
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-    
+    // Hola que ase
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
