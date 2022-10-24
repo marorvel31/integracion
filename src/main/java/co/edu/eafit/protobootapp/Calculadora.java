56ad28d2d411d4e40a2dc7a19d9b2e7af91f99c9
Mar Ortiz Velasquez <mariaaortizv@gmail.com>
2022-10-24 17:37:06 -0500

    dañando el codigo  aws

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,7 +3,7 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero + numero;
     }
     // Hola que ase
     public Double cubo(Double numero) {
